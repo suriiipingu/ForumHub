a0dd625d195d986edc0507ae7edb52a7987f0123
suriiipingu <btsherwatching@gmail.com>
2025-01-20 15:39:31 -0400

    Validação do Token--Atualização

--- src/main/java/br/com/projeto/ForumHub/ForumHubApplication.java
+++ src/main/java/br/com/projeto/ForumHub/ForumHubApplication.java
@@ -2,6 +2,8 @@ package br.com.projeto.ForumHub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ForumHubApplication {
@@ -9,5 +11,4 @@ public class ForumHubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ForumHubApplication.class, args);
 	}
-
 }
